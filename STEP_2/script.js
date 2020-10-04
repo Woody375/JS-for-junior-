7ef4380cef171df88482c1fc764a6a8e7c66c42a
Nikita <niklol375@gmail.com>
2020-10-04 21:25:42 +0300

    first commit

--- STEP_2/script.js
+++ STEP_2/script.js
@@ -1,1 +1,28 @@
-"use strict"
+
+
+// const fib = n => {
+//   let prev = 0, next = 1;
+//   for(let i = 0; i < n; i++){
+//     let temp = next;
+//     next = prev + next;
+//     prev = temp;
+//   }
+//   return prev;
+// }
+
+// console.log(fib(7));
+
+// let a = [1,4,2,8,3];
+// let max = 0;
+// let index = 0;
+//  for (let i = 0; i<5; i++){
+//      console.log(`[${i}] = ${a[i]}`);
+//      if ( a[i] > max) {
+//          max = a[i];
+//          index = i;
+//      }
+//  }
+//  let b = max + index;
+//  console.log(`Максимальный элемент = ${max}`);
+//  console.log(`Индекс максимального элемента = ${index}`);
+//  console.log(b);
